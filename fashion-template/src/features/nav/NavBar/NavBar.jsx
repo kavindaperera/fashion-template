2f92cf0fb45772f5823073ca1641f5195d78fe2d
kavindaperera <perera.kavinda52@gmail.com>
2020-04-01 16:11:55 +0530

    sticky navbar added

--- fashion-template/src/features/nav/NavBar/NavBar.jsx
+++ fashion-template/src/features/nav/NavBar/NavBar.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import {connect} from 'react-redux';
 import { withFirebase } from 'react-redux-firebase'
-import { Menu, Container, Dropdown} from "semantic-ui-react";
+import { Menu, Container, Dropdown,Visibility,} from "semantic-ui-react";
 import { NavLink, Link, withRouter } from "react-router-dom";
 import SignedOutMenu from "../Menus/SignedOutMenu";
 import SignedInMenu from "../Menus/SignedInMenu";
@@ -15,6 +15,21 @@ const mapState = (state) => ({
   auth: state.firebase.auth,
   profile: state.firebase.profile
 })
+
+const menuStyle = {
+  border: 'none',
+  borderRadius: 0,
+  boxShadow: 'none',
+  marginBottom: '1em',
+  marginTop: '4em',
+  transition: 'box-shadow 0.5s ease, padding 0.5s ease',
+}
+
+const fixedMenuStyle = {
+  backgroundColor: '#fff',
+  border: '1px solid #ddd',
+  boxShadow: '0px 3px 5px rgba(0, 0, 0, 0.2)',
+}
 
 class NavBar extends Component {
 
@@ -31,11 +46,31 @@ class NavBar extends Component {
     this.props.openModal('RegisterModal')
   }
 
+  state = {
+    menuFixed: false,
+    overlayFixed: false,
+  }
+
+  stickTopMenu = () => this.setState({ menuFixed: true })
+  unStickTopMenu = () => this.setState({ menuFixed: false })
+
+
   render() {
     const { auth, profile } = this.props;
     const authenticated = auth.isLoaded && !auth.isEmpty;
+    const { menuFixed, overlayFixed } = this.state
     return (
-      <Menu fixed="top">
+
+<Visibility
+          onBottomPassed={this.stickTopMenu}
+          onBottomVisible={this.unStickTopMenu}
+          once={false}
+        >
+
+      <Menu 
+      borderless
+            fixed={menuFixed ? 'top' : undefined}
+            style={menuFixed ? fixedMenuStyle : menuStyle}>
         <Container fluid className="nav">
           <Menu.Item as={Link} to="/" header>
             <img  src={"/company.png"} alt="logo" />
@@ -44,7 +79,6 @@ class NavBar extends Component {
           <Menu.Item name='New' as={NavLink} to="/collection"></Menu.Item>
           <Menu.Item  name='Men' as={Link} to="/"></Menu.Item>
           <Menu.Item  name='Women' as={Link} to="/"></Menu.Item>
-          <Menu.Item  name='Brands' as={Link} to="/"></Menu.Item>
           <Dropdown item simple text='Category'>
           <Dropdown.Menu>
             <Dropdown.Item>SubCategory</Dropdown.Item>
@@ -59,7 +93,7 @@ class NavBar extends Component {
             <SignedOutMenu signIn={this.handleSignedIn} register={this.handleRegister} />
           )}
         </Container>
-      </Menu>
+      </Menu></Visibility>
     );
   }
 }
